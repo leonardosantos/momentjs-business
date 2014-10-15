cb4ddf2ad7ccb1a044c418aac55b9899d363fff0
leonardosantos <leonardo.olsantos@gmail.com>
2014-10-15 01:00:17 -0300

    Update momentjs-business.js
    added support for negative numbers

--- momentjs-business.js
+++ momentjs-business.js
@@ -8,14 +8,16 @@
   moment = (typeof require !== "undefined" && require !== null) &&
            !require.amd ? require("moment") : this.moment;
 
-  moment.fn.businessDiff = function (start) {
-    start = moment(start);
-    var end = this.clone();
+  moment.fn.businessDiff = function (param) {
+    param = moment(param);
+    var signal = param.unix() < this.unix()?1:-1;
+    var start = moment.min(param, this).clone();
+    var end = moment.max(param, this).clone();
     var start_offset = start.day() - 7;
     var end_offset = end.day();
 
-    var end_sunday = end.clone().subtract('d', end_offset).startOf('day');
-    var start_sunday = start.clone().subtract('d', start_offset).startOf('day');
+    var end_sunday = end.clone().subtract('d', end_offset);
+    var start_sunday = start.clone().subtract('d', start_offset);
     var weeks = end_sunday.diff(start_sunday, 'days') / 7;
 
     start_offset = Math.abs(start_offset);
@@ -30,18 +32,24 @@
     if(end_offset == 6)
       end_offset--;
 
-    return weeks * 5 + start_offset + end_offset;
+    return signal * (weeks * 5 + start_offset + end_offset);
   };
 
   moment.fn.businessAdd = function (days) {
-    var d = this.clone().add('d', Math.floor(days / 5) * 7);
+    var signal = days<0?-1:1;
+    days = Math.abs(days);
+    var d = this.clone().add(Math.floor(days / 5) * 7 * signal, 'd');
     var remaining = days % 5;
     while(remaining){
-      d.add('d', 1);
+      d.add(signal, 'd');
       if(d.day() !== 0 && d.day() !== 6)
         remaining--;
     }
     return d;
+  };
+
+  moment.fn.businessSubtract = function(days){
+    return this.businessAdd(-days);
   };
 
 }).call(this);
